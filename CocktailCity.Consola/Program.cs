3f3cd0420c20312cd95916b64ecdbfec8a8730a7
CATT97 <cesar.tabares26@hotmail.com>
2021-11-16 20:00:15 -0500

    Inicio del proyecto

---
+++ CocktailCity.Consola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CocktailCity.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
