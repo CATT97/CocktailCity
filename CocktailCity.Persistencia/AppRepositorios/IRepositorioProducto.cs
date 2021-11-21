72c77db8a097ae0c820870af581cd88fa493ebf1
Alexis Tabares <cesar.tabares26@hotmail.com>
2021-11-21 16:46:52 -0500

    Repositorio de Productos listo

---
+++ CocktailCity.Persistencia/AppRepositorios/IRepositorioProducto.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using CocktailCity.Dominio;
+namespace CocktailCity.Persistencia
+{
+    public interface IRepositorioProducto
+    {
+         IEnumerable<Producto> GetAllProductos();
+         Producto AddProducto(Producto producto);
+         //Producto UpdateProducto(Producto producto);
+         //void DeleteProducto(int idProducto);
+         //Producto GetProducto(int idProducto);
+    }
+}
---
+++ CocktailCity.Persistencia/AppRepositorios/RepositorioProducto.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
+using CocktailCity.Dominio;
+using Microsoft.EntityFrameworkCore;
+
+namespace CocktailCity.Persistencia
+{
+    public class RepositorioProducto : IRepositorioProducto
+    {
+        private readonly AppContext _appContext = new AppContext();
+        Producto IRepositorioProducto.AddProducto(Producto producto){
+            var productoAdicionado = _appContext.Productos.Add(producto);
+            _appContext.SaveChanges();
+            return productoAdicionado.Entity;
+        }
+        IEnumerable<Producto> IRepositorioProducto.GetAllProductos(){
+            return _appContext.Productos;
+        }
+    }
+}
